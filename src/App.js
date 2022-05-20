18fba2536d8401b1aebab04ebc84909930e42182
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-20 14:06:23 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import LoginComponent from "./components/LoginComonent";
 
 function App() {
   return (
     <div className="App">
+      <LoginComponent/>
 
     </div>
   );
